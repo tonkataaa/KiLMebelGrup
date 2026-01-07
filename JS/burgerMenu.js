1fc96d2458dce35e49736e8d07bf6682c3f1925b
AntonKenderov <shikagamingbg@gmail.com>
2026-01-07 11:15:43 +0200

    Removed the old project.

--- JS/burgerMenu.js
+++
@@ -1,6 +0,0 @@
-const burger = document.getElementById("burger");
-const navItems = document.getElementById("navigation-items");
-
-burger.addEventListener("click", () => {
-    navItems.classList.toggle("show");
-});
